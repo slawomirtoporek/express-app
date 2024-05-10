41f26e8ff3755c7fdd64c30149f8665100ce9230
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-05-10 15:45:21 +0200

    add server.js and endpoint for home page

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.show('index.html');
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
